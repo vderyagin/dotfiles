8df3a9edac522ecaf2f5f6ca3715c5ab4b57e7e0
Victor Deryagin <vderyagin@gmail.com>
2013-07-01 21:37:14 +0300

    extracted folderfilter into its own function

--- .offlineimap.py
+++ .offlineimap.py
@@ -6,3 +6,10 @@ passwd_file = os.path.expanduser('~/.offlineimap-google-password.gpg')
 def get_password():
     raw_password =  subprocess.check_output(['gpg', '-d', passwd_file])
     return raw_password.strip().decode('utf-8')
+
+def filter_folders(name):
+    if name in ['[Gmail]/All Mail', '[Gmail]/Trash']:
+        return False
+    if name.startswith('mailing-lists/'):
+        return False
+    return True
