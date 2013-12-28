20c67c392bc549c11daa83dc99edc2d70b1c0ea6
Victor Deryagin <vderyagin@gmail.com>
2013-12-28 19:59:02 +0200

    do not fail when virtualenvwrapper is not installed

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,4 +1,9 @@
 if [ $EUID -ne 0 ]; then
   eval "$(rbenv init -)"
-  source '/usr/bin/virtualenvwrapper.sh'
+
+  if [ -f '/usr/bin/virtualenvwrapper.sh']; then
+    source '/usr/bin/virtualenvwrapper.sh'
+  else
+    echo 'virtualenvwrapper is not installed'
+  fi
 fi
