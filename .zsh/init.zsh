c04c6f055e6abd072e902fa44dc0a0d3eba0ce52
Victor Deryagin <vderyagin@gmail.com>
2013-12-30 10:02:17 +0200

    typo

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,7 +1,7 @@
 if [ $EUID -ne 0 ]; then
   eval "$(rbenv init -)"
 
-  if [ -f '/usr/bin/virtualenvwrapper.sh']; then
+  if [ -f '/usr/bin/virtualenvwrapper.sh' ]; then
     source '/usr/bin/virtualenvwrapper.sh'
   else
     echo 'virtualenvwrapper is not installed'
