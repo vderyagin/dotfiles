e7653eae8ee27e8cc0d28832f33bedb2457b15de
Victor Deryagin <vderyagin@gmail.com>
2014-05-05 14:29:31 +0300

    Zsh: assume rbenv is installed.

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -1,13 +1,4 @@
 if [ $EUID -ne 0 ]; then
-
-  if [ ! -d "${HOME}/.rbenv" ]; then
-    git clone "https://github.com/sstephenson/rbenv.git" "${HOME}/.rbenv"
-  fi
-
-  if [ ! -d "${HOME}/.rbenv/plugins/ruby-build" ]; then
-    git clone 'https://github.com/sstephenson/ruby-build.git' "${HOME}/.rbenv/plugins/ruby-build"
-  fi
-
   eval "$(rbenv init -)"
 
   if [ -f '/usr/bin/virtualenvwrapper.sh' ]; then
