37fe9d6d3a2b037bfb6fdeb23e6fd66fc2e17e3a
Victor Deryagin <vderyagin@gmail.com>
2014-06-19 17:53:51 +0300

    Simplified loading of stuff.

--- .zsh/init.zsh
+++ .zsh/init.zsh
@@ -2,9 +2,6 @@ if [ $EUID -ne 0 ]; then
   eval "$(rbenv init -)"
   eval "$(~/.bin/bin init)"
 
-  if [ -f '/usr/bin/virtualenvwrapper.sh' ]; then
-    source '/usr/bin/virtualenvwrapper.sh'
-  else
-    echo 'virtualenvwrapper is not installed'
-  fi
+  source "${HOME}/.travis/travis.sh"
+  source '/usr/bin/virtualenvwrapper.sh'
 fi
